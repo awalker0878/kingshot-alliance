be25eff2382e3fafbedf47e0b45969b188c8d1d4
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-08-06 23:07:20 -0400

    Add permission model

---
+++ app/Models/Permission.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+final class Permission extends Model
+{
+    use HasFactory;
+    use HasUlids;
+
+    public $incrementing = false;
+
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'key',
+        'description',
+    ];
+
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class, 'role_permissions');
+    }
+}
