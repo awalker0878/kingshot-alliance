7388ba41c77ff1b078fd5f1eb1bcdcfa14aab557
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-08-06 23:06:27 -0400

    Add global user model

---
+++ app/Models/User.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+final class User extends Authenticatable implements MustVerifyEmailContract
+{
+    use HasApiTokens;
+    use HasFactory;
+    use MustVerifyEmail;
+    use Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'timezone',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'two_factor_secret',
+        'two_factor_recovery_codes',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+            'two_factor_secret' => 'encrypted',
+            'two_factor_recovery_codes' => 'encrypted:array',
+            'two_factor_confirmed_at' => 'datetime',
+        ];
+    }
+
+    public function memberships(): HasMany
+    {
+        return $this->hasMany(AllianceMembership::class);
+    }
+}
