acea4cbb28b449c0c9e344671083606ef06555c0
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-08-06 22:16:30 -0400

    Disable Vite asset resolution in tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,4 +6,12 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
-abstract class TestCase extends BaseTestCase {}
+abstract class TestCase extends BaseTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withoutVite();
+    }
+}
