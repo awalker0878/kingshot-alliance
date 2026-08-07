8a4acc5ed373b70c30fc5fc05b4e778c3035c8d6
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-08-07 10:51:19 -0400

    Add recruitment application token service

---
+++ app/Application/Recruitment/RecruitmentApplicationTokenService.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Recruitment;
+
+final class RecruitmentApplicationTokenService
+{
+    public function issue(): string
+    {
+        return bin2hex(random_bytes(32));
+    }
+
+    public function hash(string $token): string
+    {
+        return hash('sha256', $token);
+    }
+}
