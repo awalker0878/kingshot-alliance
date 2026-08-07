37a6e9106e3b12d9652d9e62274deb038650cf4e
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-08-07 10:47:33 -0400

    Add recruitment stage transitions

---
+++ app/Domain/Recruitment/Enums/RecruitmentStage.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Recruitment\Enums;
+
+enum RecruitmentStage: string
+{
+    case New = 'new';
+    case Screening = 'screening';
+    case Contacted = 'contacted';
+    case Interview = 'interview';
+    case Accepted = 'accepted';
+    case Declined = 'declined';
+    case Withdrawn = 'withdrawn';
+    case Joined = 'joined';
+
+    /** @return list<self> */
+    public function allowedTransitions(): array
+    {
+        return match ($this) {
+            self::New => [self::Screening, self::Contacted, self::Declined, self::Withdrawn],
+            self::Screening => [self::Contacted, self::Interview, self::Accepted, self::Declined, self::Withdrawn],
+            self::Contacted => [self::Screening, self::Interview, self::Accepted, self::Declined, self::Withdrawn],
+            self::Interview => [self::Contacted, self::Accepted, self::Declined, self::Withdrawn],
+            self::Accepted => [self::Joined, self::Declined, self::Withdrawn],
+            self::Declined => [self::Screening],
+            self::Withdrawn => [self::Screening],
+            self::Joined => [],
+        };
+    }
+
+    public function canTransitionTo(self $target): bool
+    {
+        return in_array($target, $this->allowedTransitions(), true);
+    }
+
+    public function isUnsuccessful(): bool
+    {
+        return in_array($this, [self::Declined, self::Withdrawn], true);
+    }
+}
