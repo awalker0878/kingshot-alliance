b87022913f0b78034e469886c198be2d47658122
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-08-07 15:57:17 -0400

    Remove synthetic recruitment invitation model import

--- app/Domain/Recruitment/Enums/RecruitmentApplicationMode.php
+++ app/Domain/Recruitment/Enums/RecruitmentApplicationMode.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App\Domain\Recruitment\Enums;
 
-use App\Domain\Memberships\Models\Invitation;
-
 enum RecruitmentApplicationMode: string
 {
     case Public = 'public';
