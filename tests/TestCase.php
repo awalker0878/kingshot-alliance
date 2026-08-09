5e01dcae6498bdd2f2f0eb92adb5a1ace084c5e9
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-08-09 13:27:58 -0400

    Keep confirmed session fixture local to Slice D

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -14,13 +14,4 @@ abstract class TestCase extends BaseTestCase
 
         $this->withoutVite();
     }
-
-    /** @return array<string, mixed> */
-    protected function confirmedSession(string $allianceId): array
-    {
-        return [
-            (string) config('identity.active_alliance_session_key') => $allianceId,
-            'auth.password_confirmed_at' => time(),
-        ];
-    }
 }
