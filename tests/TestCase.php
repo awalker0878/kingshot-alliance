0dafb7cda727ef93ddd4ce2de80f51d65c623269
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-08-09 13:23:29 -0400

    Add confirmed privileged-session test helper

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -14,4 +14,13 @@ abstract class TestCase extends BaseTestCase
 
         $this->withoutVite();
     }
+
+    /** @return array<string, mixed> */
+    protected function confirmedSession(string $allianceId): array
+    {
+        return [
+            (string) config('identity.active_alliance_session_key') => $allianceId,
+            'auth.password_confirmed_at' => time(),
+        ];
+    }
 }
