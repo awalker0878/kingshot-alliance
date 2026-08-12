43d5ffa8d56d4b3fec8d3c7e7e53c2ad71fdd853
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-08-12 17:15:52 -0400

    Add Azure proxy header regression coverage

--- tests/Architecture/AzureContainerRuntimeTopologyTest.php
+++ tests/Architecture/AzureContainerRuntimeTopologyTest.php
@@ -21,18 +21,34 @@ final class AzureContainerRuntimeTopologyTest extends TestCase
         self::assertStringNotContainsString('fastcgi_pass app:9000;', $azure);
     }
 
-    public function test_azure_nginx_preserves_container_apps_forwarded_request_metadata(): void
+    public function test_azure_nginx_reconstructs_the_public_https_request_without_forwarded_host_or_port(): void
     {
         $azure = $this->read('docker/nginx/azure.conf');
 
         foreach ([
+            'HTTP_HOST $host',
+            'SERVER_NAME $host',
+            'SERVER_PORT 443',
+            'HTTPS on',
             'HTTP_X_FORWARDED_FOR $http_x_forwarded_for',
-            'HTTP_X_FORWARDED_HOST $http_x_forwarded_host',
-            'HTTP_X_FORWARDED_PORT $http_x_forwarded_port',
             'HTTP_X_FORWARDED_PROTO $http_x_forwarded_proto',
-        ] as $forwardedParameter) {
-            self::assertStringContainsString($forwardedParameter, $azure);
+        ] as $requestParameter) {
+            self::assertStringContainsString($requestParameter, $azure);
         }
+
+        self::assertStringNotContainsString('HTTP_X_FORWARDED_HOST $http_x_forwarded_host', $azure);
+        self::assertStringNotContainsString('HTTP_X_FORWARDED_PORT $http_x_forwarded_port', $azure);
+    }
+
+    public function test_azure_runtime_only_trusts_required_forwarded_headers(): void
+    {
+        $bootstrap = $this->read('bootstrap/app.php');
+
+        self::assertStringContainsString('Request::HEADER_X_FORWARDED_FOR', $bootstrap);
+        self::assertStringContainsString('Request::HEADER_X_FORWARDED_PROTO', $bootstrap);
+        self::assertStringNotContainsString('Request::HEADER_X_FORWARDED_HOST', $bootstrap);
+        self::assertStringNotContainsString('Request::HEADER_X_FORWARDED_PORT', $bootstrap);
+        self::assertStringNotContainsString('Request::HEADER_X_FORWARDED_PREFIX', $bootstrap);
     }
 
     public function test_runtime_image_preserves_generic_entrypoint_and_packages_azure_nginx_profile(): void
