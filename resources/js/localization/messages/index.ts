ca59ef7b0c64c0ffeff64f070e2cdd404ad810be
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-08-13 13:21:20 -0400

    Wire UX-P8 platform localization catalogue

--- resources/js/localization/messages/index.ts
+++ resources/js/localization/messages/index.ts
@@ -17,6 +17,7 @@ import { kingdomP7AMessages } from './kingdom-p7a';
 import { kingdomP7BMessages } from './kingdom-p7b';
 import { kingdomP7CMessages } from './kingdom-p7c';
 import { kingdomP7DMessages } from './kingdom-p7d';
+import { platformP8Messages } from './platform-p8';
 import { publicExtraMessages } from './public-extra';
 import { publicMessages } from './public';
 import { recruitmentMessages } from './recruitment';
@@ -170,6 +171,7 @@ type KingdomP7AMessageTree = (typeof kingdomP7AMessages)['en'];
 type KingdomP7BMessageTree = (typeof kingdomP7BMessages)['en'];
 type KingdomP7CMessageTree = (typeof kingdomP7CMessages)['en'];
 type KingdomP7DMessageTree = (typeof kingdomP7DMessages)['en'];
+type PlatformP8MessageTree = (typeof platformP8Messages)['en'];
 type AccountExperienceMessageTree = (typeof accountExperienceMessages)['en'];
 type AllianceOperationsMessageTree = (typeof allianceOperationsMessages)['en'];
 type ApplicationExtraMessageTree = (typeof applicationExtraMessages)['en'];
@@ -191,6 +193,7 @@ export type MessageTree = BaseMessageTree &
   KingdomP7BMessageTree &
   KingdomP7CMessageTree &
   KingdomP7DMessageTree &
+  PlatformP8MessageTree &
   AccountExperienceMessageTree &
   AllianceOperationsMessageTree &
   ApplicationExtraMessageTree &
@@ -219,6 +222,7 @@ function catalogue(locale: LocaleCode) {
     ...kingdomP7BMessages[locale],
     ...kingdomP7CMessages[locale],
     ...kingdomP7DMessages[locale],
+    ...platformP8Messages[locale],
     ...accountExperienceMessages[locale],
     ...allianceOperationsMessages[locale],
     ...applicationExtraMessages[locale],
