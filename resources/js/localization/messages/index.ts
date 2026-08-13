5f55b3e1357f1f3932508a2f82e39cd78c5d33e1
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-08-12 22:25:15 -0400

    Merge public locale catalogues

--- resources/js/localization/messages/index.ts
+++ resources/js/localization/messages/index.ts
@@ -1,17 +1,34 @@
 import type { LocaleCode } from '../locales';
 import { additionalCatalogues } from './catalogues';
 import en from './en';
+import { publicExtraMessages } from './public-extra';
+import { publicMessages } from './public';
 
 type StringLeaves<T> = {
   [K in keyof T]: T[K] extends string ? string : T[K] extends object ? StringLeaves<T[K]> : never;
 };
 
-export type MessageTree = StringLeaves<typeof en>;
+type BaseMessageTree = StringLeaves<typeof en>;
+type PublicMessageTree = (typeof publicMessages)['en'];
+type PublicExtraMessageTree = (typeof publicExtraMessages)['en'];
 
-export const messages: Record<LocaleCode, MessageTree> = {
+export type MessageTree = BaseMessageTree & PublicMessageTree & PublicExtraMessageTree;
+
+const baseMessages: Record<LocaleCode, BaseMessageTree> = {
   en,
   ...additionalCatalogues,
 };
+
+export const messages = Object.fromEntries(
+  (Object.keys(baseMessages) as LocaleCode[]).map((locale) => [
+    locale,
+    {
+      ...baseMessages[locale],
+      ...publicMessages[locale],
+      ...publicExtraMessages[locale],
+    },
+  ]),
+) as Record<LocaleCode, MessageTree>;
 
 export function hasMessageCatalogue(locale: LocaleCode): boolean {
   return Object.prototype.hasOwnProperty.call(messages, locale);
