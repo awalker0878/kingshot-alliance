e7f58f81990eef364a715ed7fe86d1ec854890ee
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-08-15 12:07:04 -0400

    ARCH-V2-P2: add account identity model

---
+++ app/Contexts/Accounts/Models/User.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Contexts\Accounts\Models;
+
+use Database\Factories\UserFactory;
+use Illuminate\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Carbon;
+use Laravel\Sanctum\HasApiTokens;
+
+/**
+ * Global account identity. Game authority belongs to the active Player, not User.
+ *
+ * @property string|null $two_factor_secret
+ * @property list<string>|null $two_factor_recovery_codes
+ * @property Carbon|null $two_factor_confirmed_at
+ * @property Carbon|null $email_verified_at
+ * @property Carbon|null $deletion_requested_at
+ * @property Carbon|null $anonymized_at
+ */
+final class User extends Authenticatable implements MustVerifyEmailContract
+{
+    use HasApiTokens;
+
+    /** @use HasFactory<UserFactory> */
+    use HasFactory;
+
+    use MustVerifyEmail;
+    use Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'timezone',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'two_factor_secret',
+        'two_factor_recovery_codes',
+    ];
+
+    protected static function newFactory(): UserFactory
+    {
+        return UserFactory::new();
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+            'two_factor_secret' => 'encrypted',
+            'two_factor_recovery_codes' => 'encrypted:array',
+            'two_factor_confirmed_at' => 'datetime',
+            'deletion_requested_at' => 'datetime',
+            'anonymized_at' => 'datetime',
+        ];
+    }
+}
