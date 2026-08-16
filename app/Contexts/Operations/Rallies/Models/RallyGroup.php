0c10184b981378cb043b66136514c7f95bc60174
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-08-15 22:37:35 -0400

    ARCH-V2: type RallyGroup aggregate relations

--- app/Contexts/Operations/Rallies/Models/RallyGroup.php
+++ app/Contexts/Operations/Rallies/Models/RallyGroup.php
@@ -11,6 +11,11 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * @property-read EventOccurrence $occurrence
+ * @property-read Alliance $alliance
+ * @property-read EventRecommendedFormation|null $recommendedFormation
+ */
 final class RallyGroup extends Model
 {
     use HasUlids;
