258a89efe9154c26e4572f90cfd4ea6a4b9e6715
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-08-16 00:56:11 -0400

    refactor: keep default alliance roles identity-only

--- app/Contexts/Alliance/Access/Enums/DefaultAllianceRole.php
+++ app/Contexts/Alliance/Access/Enums/DefaultAllianceRole.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace App\Contexts\Alliance\Access\Enums;
 
-use App\Shared\Access\Contracts\Permission;
-use App\Shared\Access\ValueObjects\NamedPermission;
-
 enum DefaultAllianceRole: string
 {
     case Recruiter = 'recruiter';
@@ -19,22 +16,6 @@ enum DefaultAllianceRole: string
             self::Recruiter => 'Recruiter',
             self::EventCoordinator => 'Event Coordinator',
             self::ContentManager => 'Content Manager',
-        };
-    }
-
-    /** @return list<Permission> */
-    public function permissions(): array
-    {
-        return match ($this) {
-            self::Recruiter => [
-                AlliancePermission::InvitationManage,
-                AlliancePermission::RecruitmentManage,
-            ],
-            self::EventCoordinator => [
-                NamedPermission::from('events.alliance.create'),
-                NamedPermission::from('events.alliance.manage'),
-            ],
-            self::ContentManager => [AlliancePermission::ContentManage],
         };
     }
 }
