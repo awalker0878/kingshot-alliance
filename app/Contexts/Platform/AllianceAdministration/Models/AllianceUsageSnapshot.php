a4a9269b0b0e28e0749af4e15c27024afcb7d57f
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-08-16 17:56:24 -0400

    refactor(v3): add Platform alliance administration capability

---
+++ app/Contexts/Platform/AllianceAdministration/Models/AllianceUsageSnapshot.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Contexts\Platform\AllianceAdministration\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Model;
+
+final class AllianceUsageSnapshot extends Model
+{
+    use HasUlids;
+    public $incrementing = false;
+    protected $keyType = 'string';
+    protected $fillable = ['alliance_id', 'active_members', 'storage_bytes', 'active_api_credentials', 'active_webhook_subscriptions', 'pending_outbox_messages', 'captured_at'];
+
+    protected function casts(): array
+    {
+        return ['active_members' => 'integer', 'storage_bytes' => 'integer', 'active_api_credentials' => 'integer', 'active_webhook_subscriptions' => 'integer', 'pending_outbox_messages' => 'integer', 'captured_at' => 'datetime'];
+    }
+}
