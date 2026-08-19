59abd2376a900b250b310472d07cebef6ebf8dc4
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-08-19 05:33:00 -0400

    retarget King Perks build contract to Royal Court

--- vite.king-perks.config.ts
+++ vite.king-perks.config.ts
@@ -8,8 +8,8 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        manage: 'resources/js/pages/KingPerks/Manage.vue',
-        my: 'resources/js/pages/KingPerks/My.vue',
+        manage: 'resources/js/pages/Kingdom/RoyalCourt/Appointments.vue',
+        my: 'resources/js/pages/Kingdom/RoyalCourt/MyAppointments.vue',
       },
     },
   },
