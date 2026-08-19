7a7225650dff20ad054836903cafd331a1bafec0
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-08-19 05:58:04 -0400

    add context-aware Governor link component

---
+++ resources/js/components/navigation/GovernorContextLink.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { useGovernorNavigation } from '@/composables/useGovernorNavigation';
+
+const props = defineProps<{
+  governorId: string;
+  href: string;
+}>();
+
+const { visit } = useGovernorNavigation();
+
+function navigate(event: MouseEvent): void {
+  if (
+    event.defaultPrevented ||
+    event.button !== 0 ||
+    event.metaKey ||
+    event.ctrlKey ||
+    event.shiftKey ||
+    event.altKey
+  ) {
+    return;
+  }
+
+  event.preventDefault();
+  visit({ governorId: props.governorId, path: props.href });
+}
+</script>
+
+<template>
+  <a :href="href" @click="navigate">
+    <slot />
+  </a>
+</template>
