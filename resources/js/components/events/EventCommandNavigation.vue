964aba0aa872a98878da6f9be9c05e00bd3b22c8
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-08-19 05:33:16 -0400

    format Event Command navigation

--- resources/js/components/events/EventCommandNavigation.vue
+++ resources/js/components/events/EventCommandNavigation.vue
@@ -12,9 +12,15 @@ function has(capability: string): boolean {
 <template>
   <nav class="ks-tab-strip mt-4" aria-label="Event organizer command sections">
     <a href="#schedule" class="ks-tab">{{ t('events.manage.title') }}</a>
-    <a v-if="has('phases')" href="#phases" class="ks-tab">{{ t('events.phases.manageTitle') }}</a>
-    <a v-if="has('polls')" href="#polls" class="ks-tab">{{ t('events.polls.manageTitle') }}</a>
-    <a v-if="has('rosters')" href="#rosters" class="ks-tab">{{ t('events.rosters.manageTitle') }}</a>
+    <a v-if="has('phases')" href="#phases" class="ks-tab">
+      {{ t('events.phases.manageTitle') }}
+    </a>
+    <a v-if="has('polls')" href="#polls" class="ks-tab">
+      {{ t('events.polls.manageTitle') }}
+    </a>
+    <a v-if="has('rosters')" href="#rosters" class="ks-tab">
+      {{ t('events.rosters.manageTitle') }}
+    </a>
     <a v-if="has('rally_guidance') || has('formations')" href="#rallies" class="ks-tab">
       {{ t('events.rallies.manageTitle') }}
     </a>
@@ -24,7 +30,11 @@ function has(capability: string): boolean {
     <a v-if="has('results')" href="#results" class="ks-tab">
       {{ t('events.results.manageTitle') }}
     </a>
-    <a v-if="has('responses') || has('attendance')" href="#participants" class="ks-tab">
+    <a
+      v-if="has('responses') || has('attendance')"
+      href="#participants"
+      class="ks-tab"
+    >
       {{ t('events.manage.participants') }}
     </a>
     <a href="#reminders" class="ks-tab">{{ t('events.manage.reminders') }}</a>
