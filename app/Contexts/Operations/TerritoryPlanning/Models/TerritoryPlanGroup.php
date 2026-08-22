9feb13f29a3e9c72adbc294c3fe64e69e7a6653f
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-08-22 07:29:51 -0400

    Type Territory group persistence contract

--- app/Contexts/Operations/TerritoryPlanning/Models/TerritoryPlanGroup.php
+++ app/Contexts/Operations/TerritoryPlanning/Models/TerritoryPlanGroup.php
@@ -7,6 +7,11 @@ namespace App\Contexts\Operations\TerritoryPlanning\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string $id
+ * @property string $territory_plan_id
+ * @property string|null $label
+ */
 final class TerritoryPlanGroup extends Model
 {
     use HasUlids;
