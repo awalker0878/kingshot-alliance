4bc6a0f60a45bcfbca21d685e1331f321a151da3
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-08-22 07:30:53 -0400

    Type Event Territory revision link contract

--- app/Contexts/Operations/TerritoryPlanning/Models/EventTerritoryPlanRevision.php
+++ app/Contexts/Operations/TerritoryPlanning/Models/EventTerritoryPlanRevision.php
@@ -6,7 +6,16 @@ namespace App\Contexts\Operations\TerritoryPlanning\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
+/**
+ * @property string $id
+ * @property string $event_occurrence_id
+ * @property string $territory_plan_revision_id
+ * @property string $purpose
+ * @property string $created_by_player_id
+ * @property Carbon $created_at
+ */
 final class EventTerritoryPlanRevision extends Model
 {
     use HasUlids;
