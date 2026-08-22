9bbc9d52e13e963f3db10c567d1eec2e22add1a6
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-08-22 07:29:46 -0400

    Type Territory Alliance layer persistence contract

--- app/Contexts/Operations/TerritoryPlanning/Models/TerritoryPlanAlliance.php
+++ app/Contexts/Operations/TerritoryPlanning/Models/TerritoryPlanAlliance.php
@@ -7,6 +7,18 @@ namespace App\Contexts\Operations\TerritoryPlanning\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string $id
+ * @property string $territory_plan_id
+ * @property string|null $alliance_id
+ * @property string|null $external_name
+ * @property string|null $external_tag
+ * @property string $display_name
+ * @property string $presentation_color
+ * @property int $sort_order
+ * @property bool $visible
+ * @property bool $locked
+ */
 final class TerritoryPlanAlliance extends Model
 {
     use HasUlids;
