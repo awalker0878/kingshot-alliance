a355faa1d05bb21cb1c3169fff56912169906fd3
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-08-22 11:26:52 -0400

    Route Kingdom Territory Alliance layer management

--- app/ReadModels/TerritoryPlanning/Providers/TerritoryPlanningReadModelServiceProvider.php
+++ app/ReadModels/TerritoryPlanning/Providers/TerritoryPlanningReadModelServiceProvider.php
@@ -15,6 +15,9 @@ final class TerritoryPlanningReadModelServiceProvider extends ServiceProvider
         Route::middleware(['web', 'auth', 'auth.session', 'verified'])->group(function (): void {
             Route::get('/territory', [TerritoryPlanningPageController::class, 'index'])
                 ->name('territory.index');
+            Route::get('/territory/{plan}/alliances', [TerritoryPlanningPageController::class, 'alliances'])
+                ->whereUlid('plan')
+                ->name('territory.alliances');
             Route::get('/territory/{plan}', [TerritoryPlanningPageController::class, 'show'])
                 ->whereUlid('plan')
                 ->name('territory.show');
