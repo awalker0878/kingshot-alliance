de611afe7a07f3844d95962978a9077883e81494
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-08-22 07:27:52 -0400

    Register Territory planning read-model routes

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -15,6 +15,7 @@ use App\Contexts\Operations\Participation\Providers\ParticipationServiceProvider
 use App\Contexts\Operations\TerritoryPlanning\Providers\TerritoryPlanningServiceProvider;
 use App\Contexts\Platform\Administration\Providers\AdministrationServiceProvider;
 use App\Contexts\Platform\Integrations\Providers\IntegrationsServiceProvider;
+use App\ReadModels\TerritoryPlanning\Providers\TerritoryPlanningReadModelServiceProvider;
 use App\Shared\Infrastructure\Providers\InfrastructureServiceProvider;
 
 return [
@@ -30,6 +31,7 @@ return [
     ParticipationServiceProvider::class,
     KingPerksServiceProvider::class,
     TerritoryPlanningServiceProvider::class,
+    TerritoryPlanningReadModelServiceProvider::class,
     AdministrationServiceProvider::class,
     IntegrationsServiceProvider::class,
 ];
