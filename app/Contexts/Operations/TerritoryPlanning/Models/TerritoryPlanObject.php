41262dac6fa257ccf8fa0fd079b0fc5e37725eba
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-08-22 07:29:36 -0400

    Type Territory object persistence contract

--- app/Contexts/Operations/TerritoryPlanning/Models/TerritoryPlanObject.php
+++ app/Contexts/Operations/TerritoryPlanning/Models/TerritoryPlanObject.php
@@ -8,6 +8,21 @@ use App\Contexts\Operations\TerritoryPlanning\Enums\TerritoryObjectType;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string $id
+ * @property string $territory_plan_id
+ * @property string $territory_plan_alliance_id
+ * @property string|null $group_id
+ * @property TerritoryObjectType $object_type
+ * @property string|null $player_id
+ * @property string|null $external_player_name
+ * @property string|null $label
+ * @property int $coordinate_x
+ * @property int $coordinate_y
+ * @property int $rotation
+ * @property int $sort_order
+ * @property array<string, mixed>|null $metadata
+ */
 final class TerritoryPlanObject extends Model
 {
     use HasUlids;
