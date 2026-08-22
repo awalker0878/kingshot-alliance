95b5cb3f283ad052dfe02fff88c73a1b7208ccfd
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-08-22 07:29:28 -0400

    Type Territory revision persistence contract

--- app/Contexts/Operations/TerritoryPlanning/Models/TerritoryPlanRevision.php
+++ app/Contexts/Operations/TerritoryPlanning/Models/TerritoryPlanRevision.php
@@ -6,7 +6,21 @@ namespace App\Contexts\Operations\TerritoryPlanning\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
+/**
+ * @property string $id
+ * @property string $territory_plan_id
+ * @property int $revision_number
+ * @property int $schema_version
+ * @property string $map_dataset_id
+ * @property string $map_dataset_checksum
+ * @property array<string, mixed> $snapshot
+ * @property string $snapshot_checksum
+ * @property string $published_by_player_id
+ * @property Carbon $published_at
+ * @property Carbon $created_at
+ */
 final class TerritoryPlanRevision extends Model
 {
     use HasUlids;
