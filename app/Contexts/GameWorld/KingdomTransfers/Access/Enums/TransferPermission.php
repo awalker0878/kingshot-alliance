d078f7032fd062f0e11b6da35b16e5ee88662ad4
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-08-23 13:32:57 -0400

    fix: add kingdom transfer view permission

--- app/Contexts/GameWorld/KingdomTransfers/Access/Enums/TransferPermission.php
+++ app/Contexts/GameWorld/KingdomTransfers/Access/Enums/TransferPermission.php
@@ -6,5 +6,6 @@ namespace App\Contexts\GameWorld\KingdomTransfers\Access\Enums;
 
 enum TransferPermission: string
 {
+    case View = 'kingdom_transfer.view';
     case Manage = 'kingdom_transfer.manage';
 }
