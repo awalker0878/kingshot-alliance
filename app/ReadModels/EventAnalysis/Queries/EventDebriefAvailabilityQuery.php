1965da2b7fe2f428d99208a369a1324b72e1eb2d
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-08-24 16:08:09 -0700

    Format Event debrief availability query

--- app/ReadModels/EventAnalysis/Queries/EventDebriefAvailabilityQuery.php
+++ app/ReadModels/EventAnalysis/Queries/EventDebriefAvailabilityQuery.php
@@ -13,17 +13,21 @@ final readonly class EventDebriefAvailabilityQuery
 {
     public function __construct(private BearHuntDebriefResultQuery $bearHuntResults) {}
 
-    /**
-     * @return array{supported:bool,available:bool,href:?string}
-     */
+    /** @return array{supported:bool,available:bool,href:?string} */
     public function forOccurrence(EventOccurrence $occurrence): array
     {
         $occurrence->loadMissing('event.eventType');
         $event = $occurrence->event;
-        if (! $event instanceof Event
+        if (
+            ! $event instanceof Event
             || $event->scopeEnum() !== EventScope::Alliance
-            || $event->eventType?->slug !== 'bear-hunt') {
-            return ['supported' => false, 'available' => false, 'href' => null];
+            || $event->eventType?->slug !== 'bear-hunt'
+        ) {
+            return [
+                'supported' => false,
+                'available' => false,
+                'href' => null,
+            ];
         }
 
         $results = $this->bearHuntResults->forOccurrence((string) $occurrence->id);
