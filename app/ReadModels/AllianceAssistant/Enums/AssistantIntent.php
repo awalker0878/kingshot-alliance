80d7a02c574662f36e31242f3033300b2971662f
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-08-24 12:32:40 -0700

    feat: add bounded Alliance Assistant intents

--- app/ReadModels/AllianceAssistant/Enums/AssistantIntent.php
+++ app/ReadModels/AllianceAssistant/Enums/AssistantIntent.php
@@ -9,7 +9,13 @@ enum AssistantIntent: string
     case Help = 'help';
     case EventTime = 'event_time';
     case EventRosterSelf = 'event_roster_self';
+    case EventParticipationSelf = 'event_participation_self';
+    case BattlePlanSelf = 'battle_plan_self';
+    case GameFact = 'game_fact';
+    case TransferStatusSelf = 'transfer_status_self';
+    case TerritoryPlan = 'territory_plan';
     case AllianceContent = 'alliance_content';
     case AllianceObservation = 'alliance_observation';
+    case ActionHandoff = 'action_handoff';
     case Unsupported = 'unsupported';
 }
