4901a6644d89a7708333b43e62fa52e016de622c
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-08-27 17:34:14 -0700

    Declare spatial observation model types

--- app/Contexts/Intelligence/Observations/Models/SpatialObservation.php
+++ app/Contexts/Intelligence/Observations/Models/SpatialObservation.php
@@ -6,10 +6,19 @@ namespace App\Contexts\Intelligence\Observations\Models;
 
 use App\Contexts\Intelligence\Observations\Enums\SpatialObservationCompleteness;
 use App\Contexts\Intelligence\Observations\Enums\SpatialObservationCoverageKind;
+use Carbon\CarbonInterface;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * @property CarbonInterface $captured_at
+ * @property SpatialObservationCoverageKind $coverage_kind
+ * @property SpatialObservationCompleteness $completeness
+ * @property array{x:int,y:int,width:int,height:int}|null $coverage_bounds
+ * @property CarbonInterface|null $accepted_at
+ * @property CarbonInterface|null $invalidated_at
+ */
 final class SpatialObservation extends Model
 {
     use HasUlids;
