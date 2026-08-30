69e51cace3aa311ae6a6ea1db56bc89f1c658cdb
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-08-30 05:31:12 -0700

    refactor: remove legacy gift code evidence class

--- app/Contexts/GameWorld/GiftCodes/Enums/GiftCodeEvidenceClassification.php
+++ app/Contexts/GameWorld/GiftCodes/Enums/GiftCodeEvidenceClassification.php
@@ -7,7 +7,6 @@ namespace App\Contexts\GameWorld\GiftCodes\Enums;
 enum GiftCodeEvidenceClassification: string
 {
     case CommunityClaim = 'community_claim';
-    case LegacyClaim = 'legacy_claim';
     case OfficialPublication = 'official_publication';
     case IndependentObservation = 'independent_observation';
     case ProviderOutcome = 'provider_outcome';
