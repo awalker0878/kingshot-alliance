6d4ac315931dc19bb43a97c7555923ea7d50d4ef
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-09-04 09:51:25 -0700

    types: annotate permission model

--- app/Shared/Infrastructure/Access/Models/Permission.php
+++ app/Shared/Infrastructure/Access/Models/Permission.php
@@ -7,6 +7,11 @@ namespace App\Shared\Infrastructure\Access\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string $id
+ * @property string $key
+ * @property string $description
+ */
 final class Permission extends Model
 {
     use HasUlids;
