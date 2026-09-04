8f69e7d21e45175eb3ea467edaa759570981f06e
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-09-04 13:03:26 -0700

    feat: extend communications delivery channels

--- app/Contexts/Communications/Delivery/Enums/DeliveryChannel.php
+++ app/Contexts/Communications/Delivery/Enums/DeliveryChannel.php
@@ -9,6 +9,8 @@ enum DeliveryChannel: string
     case InApp = 'in_app';
     case Discord = 'discord';
     case Telegram = 'telegram';
+    case WebPush = 'web_push';
+    case Email = 'email';
 
     public function label(): string
     {
@@ -16,11 +18,21 @@ enum DeliveryChannel: string
             self::InApp => 'In app',
             self::Discord => 'Discord',
             self::Telegram => 'Telegram',
+            self::WebPush => 'Web Push',
+            self::Email => 'Email',
         };
     }
 
     public function isExternal(): bool
     {
         return $this !== self::InApp;
+    }
+
+    public function usesStoredEndpoint(): bool
+    {
+        return match ($this) {
+            self::Discord, self::Telegram, self::WebPush => true,
+            self::InApp, self::Email => false,
+        };
     }
 }
