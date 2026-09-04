898c96fa1b674ba8cffb73c1cacf93b9f0fd4d34
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-09-04 13:05:53 -0700

    feat: add notification queue receipt

---
+++ app/Contexts/Communications/Delivery/ValueObjects/NotificationQueueReceipt.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Contexts\Communications\Delivery\ValueObjects;
+
+final readonly class NotificationQueueReceipt
+{
+    /**
+     * @param list<string> $deliveryIds
+     * @param list<string> $channels
+     * @param list<string> $createdDeliveryIds
+     */
+    public function __construct(
+        public string $messageId,
+        public array $deliveryIds,
+        public array $channels,
+        public array $createdDeliveryIds,
+        public bool $createdMessage,
+    ) {}
+
+    public function count(): int
+    {
+        return count($this->deliveryIds);
+    }
+
+    public function hasCreatedDeliveries(): bool
+    {
+        return $this->createdDeliveryIds !== [];
+    }
+}
