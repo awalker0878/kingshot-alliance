94d119a9a126e059421669738137680e20889d82
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-09-05 14:21:55 -0700

    Add Gift Code extracted evidence value object

---
+++ app/Contexts/GameWorld/GiftCodes/ValueObjects/GiftCodeExtractedEvidence.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Contexts\GameWorld\GiftCodes\ValueObjects;
+
+final readonly class GiftCodeExtractedEvidence
+{
+    /** @param array<string,mixed>|null $applicability @param array<string,mixed>|null $reward */
+    public function __construct(
+        public string $code,
+        public ?string $claimedExpiresAt,
+        public ?string $expiryPrecision,
+        public ?string $expiryTimezone,
+        public ?array $applicability,
+        public ?array $reward,
+    ) {}
+}
