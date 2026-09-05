c59ffd8f1c346b50d978b283af3afaea8097ff4c
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-09-05 06:00:19 -0700

    Register researched platform Gift Code adapters

--- app/Contexts/GameWorld/GiftCodes/Providers/GiftCodesServiceProvider.php
+++ app/Contexts/GameWorld/GiftCodes/Providers/GiftCodesServiceProvider.php
@@ -5,10 +5,15 @@ declare(strict_types=1);
 namespace App\Contexts\GameWorld\GiftCodes\Providers;
 
 use App\Contexts\GameWorld\GiftCodes\Adapters\CenturyGamesKingshotNewsRssGiftCodeSourceAdapter;
+use App\Contexts\GameWorld\GiftCodes\Adapters\DiscordChannelGiftCodeSourceAdapter;
+use App\Contexts\GameWorld\GiftCodes\Adapters\FacebookPageGiftCodeSourceAdapter;
+use App\Contexts\GameWorld\GiftCodes\Adapters\InstagramMediaGiftCodeSourceAdapter;
 use App\Contexts\GameWorld\GiftCodes\Adapters\JsonFeedGiftCodeSourceAdapter;
 use App\Contexts\GameWorld\GiftCodes\Adapters\OfficialXGiftCodeSourceAdapter;
+use App\Contexts\GameWorld\GiftCodes\Adapters\RedditSubredditGiftCodeSourceAdapter;
 use App\Contexts\GameWorld\GiftCodes\Adapters\RssAtomGiftCodeSourceAdapter;
 use App\Contexts\GameWorld\GiftCodes\Adapters\StructuredHtmlGiftCodeSourceAdapter;
+use App\Contexts\GameWorld\GiftCodes\Adapters\YouTubeChannelGiftCodeSourceAdapter;
 use App\Contexts\GameWorld\GiftCodes\Contracts\GiftCodeRedemptionProvider;
 use App\Contexts\GameWorld\GiftCodes\Services\GiftCodeSourceAdapterRegistry;
 use App\Contexts\GameWorld\GiftCodes\Services\OfficialGiftCodeHandoff;
@@ -23,12 +28,22 @@ final class GiftCodesServiceProvider extends ServiceProvider
         $this->app->singleton(StructuredHtmlGiftCodeSourceAdapter::class);
         $this->app->singleton(OfficialXGiftCodeSourceAdapter::class);
         $this->app->singleton(CenturyGamesKingshotNewsRssGiftCodeSourceAdapter::class);
+        $this->app->singleton(DiscordChannelGiftCodeSourceAdapter::class);
+        $this->app->singleton(YouTubeChannelGiftCodeSourceAdapter::class);
+        $this->app->singleton(RedditSubredditGiftCodeSourceAdapter::class);
+        $this->app->singleton(FacebookPageGiftCodeSourceAdapter::class);
+        $this->app->singleton(InstagramMediaGiftCodeSourceAdapter::class);
         $this->app->tag([
             JsonFeedGiftCodeSourceAdapter::class,
             RssAtomGiftCodeSourceAdapter::class,
             StructuredHtmlGiftCodeSourceAdapter::class,
             OfficialXGiftCodeSourceAdapter::class,
             CenturyGamesKingshotNewsRssGiftCodeSourceAdapter::class,
+            DiscordChannelGiftCodeSourceAdapter::class,
+            YouTubeChannelGiftCodeSourceAdapter::class,
+            RedditSubredditGiftCodeSourceAdapter::class,
+            FacebookPageGiftCodeSourceAdapter::class,
+            InstagramMediaGiftCodeSourceAdapter::class,
         ], 'gift-code-source-adapter');
         $this->app->singleton(
             GiftCodeSourceAdapterRegistry::class,
