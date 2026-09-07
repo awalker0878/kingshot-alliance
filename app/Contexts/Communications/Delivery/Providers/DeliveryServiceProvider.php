46a0f7b0e2fda76eb6d858da29bc27a1ed9278b2
awalker0878 <52688461+awalker0878@users.noreply.github.com>
2026-09-07 07:57:25 -0700

    Keep delivery provider scoped to delivery commands

--- app/Contexts/Communications/Delivery/Providers/DeliveryServiceProvider.php
+++ app/Contexts/Communications/Delivery/Providers/DeliveryServiceProvider.php
@@ -7,8 +7,6 @@ namespace App\Contexts\Communications\Delivery\Providers;
 use App\Contexts\Communications\Delivery\Console\Commands\BuildNotificationDigestsCommand;
 use App\Contexts\Communications\Delivery\Console\Commands\ProcessNotificationDeliveriesCommand;
 use App\Contexts\Communications\Delivery\Console\Commands\ProcessNotificationDigestsCommand;
-use App\Contexts\Communications\Delivery\Console\Commands\QueueIntelligenceChangesCommand;
-use App\Contexts\Communications\Delivery\Console\Commands\QueueOfficerBriefsCommand;
 use Illuminate\Support\ServiceProvider;
 
 final class DeliveryServiceProvider extends ServiceProvider
@@ -20,8 +18,6 @@ final class DeliveryServiceProvider extends ServiceProvider
                 BuildNotificationDigestsCommand::class,
                 ProcessNotificationDeliveriesCommand::class,
                 ProcessNotificationDigestsCommand::class,
-                QueueIntelligenceChangesCommand::class,
-                QueueOfficerBriefsCommand::class,
             ]);
         }
     }
